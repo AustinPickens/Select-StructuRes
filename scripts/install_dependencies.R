4f4cb5f11e8637d6806d63dbe81d7bbaa3b67df4
Austin Pickens <Austinpickens86@gmail.com>
2019-03-18 07:25:48 -0400

    Update for newer R versions
    fix installer issue with R version 5 and up

--- scripts/install_dependencies.R
+++ scripts/install_dependencies.R
@@ -4,7 +4,15 @@
   if(length(new.packages)){
     install.packages(new.packages[new.packages!='ChemmineR'])
     if('ChemmineR' %in% new.packages) {
-      source("https://bioconductor.org/biocLite.R")
-      biocLite("ChemmineR")
+      if(version[7] < 5){ # This should work with R pre-version 5
+        source("https://bioconductor.org/biocLite.R")
+        biocLite("ChemmineR")
+       } else { # This should work with R version 5 and above
+        if (!requireNamespace("BiocManager", quietly = TRUE))
+          install.packages("BiocManager")
+          BiocManager::install("ChemmineR", version = "3.8")
+       }      
     }
 }
+
+## If you have any issues it is probably realted to your version of R and the bioconductor installer/ChemmineR
